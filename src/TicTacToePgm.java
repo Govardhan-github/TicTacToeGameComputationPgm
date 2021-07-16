2004e998536ef442f4642615722a84c9553642c2
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-16 11:20:51 -0700

    ADDED : Giving Input Method For Tic Tac Toe Game

--- src/TicTacToePgm.java
+++ src/TicTacToePgm.java
@@ -1,3 +1,5 @@
+//Importing java util package
+import java.util.*;
 
 public class TicTacToePgm {
 	/*
@@ -5,20 +7,39 @@ public class TicTacToePgm {
 	 */
 	public static char[] createBoard() {
 	/*
-	 * Creating Board with size 10
+	 * Creating Board array with size 10
 	 */
-	char[] board = new char[10];
-	//Assigning empty space for each value
-	//neglecting zero index from board
-	for(int i = 1;i < board.length; i++) {
-		board[i]= ' ';} 
+		char[] board = new char[10];
+		//Assigning empty space for each value
+		//neglecting zero index from board
+		for(int i = 1;i < board.length; i++) {
+		board[i]= ' ';
+		} 
 		return board;
+		}
+	
+		/*
+		 * Creating giving input method to give user input
+		 */
+	public static char givingInput() {	
+		//Determining the player letter and computer letter
+		System.out.println("Computer letter is X ");
+		System.out.println("Player letter is O ");
+		//Scanner package to take input from user
+		Scanner sc =new Scanner(System.in); 
+		System.out.println("Enter the X or O character to play :" );
+		char input =sc.next().charAt(0);
+		System.out.println("Entered Character is:" +input );
+		return input;
 		}	
-	/*
-	 * Typing main method here
-	 * Calling createBoard method in main method
-	 */
+		
+		/*
+		 * Declaring main method here
+		 * Calling createBoard method in main method
+		 * Calling giving input method in main method
+		 */
 	public static void main(String[] args) {
 		char[] board=createBoard();
+		givingInput();
 	}
 }
